40f097ab5d5eab2213aab81040c18c462cc18494
Esseberry <rvipurnama29@gmail.com>
2024-07-09 22:11:32 +0700

    side bar

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class BarangController extends Controller
 {
-    //
+    public function view(Request $request): Response
+    {
+        return Inertia::render('DashboardPages/BarangItem');
+    }
 }
